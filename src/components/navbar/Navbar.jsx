904675fc5338e2cda7cf97054aa81f552e3dcd53
Raul Villar <raultheviking@gmail.com>
2023-04-11 10:02:52 +0200

    Fixed navbar and background responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   const [results, setResults] = useState([]);
   const [showResults, setShowResults] = useState(false);
   const httpService = HTTPService();
-  
+
 
 
 
@@ -42,65 +42,48 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-dark">
-      <div className="container-fluid">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+      <div class="container-fluid">
         <Link to="/">
-          <img className="navbar-brand logo" src={logo} alt="logo" />
+          <img class="navbar-brand" src={logo} alt="logo" style={{ width: "55vw" }}></img>
         </Link>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle text-white"
-                href="/"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Menu
               </a>
-              <ul className="dropdown-menu bg-dark">
-                <li>
-                  <Link to="/leyendas" className="dropdown-item text-white">
-                    Experiencias paranormales
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/historias" className="dropdown-item text-white">
-                    Tus historias
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/psicofonias"
-                    className="dropdown-item text-white"
-                  >
-                    Psicofonías
-                  </Link>
-                </li>
+              <ul class="dropdown-menu bg-dark">
+                <Link to="/leyendas">
+                  <li><a class="dropdown-item text-white">Experiencias paranormales</a></li>
+                </Link>
+                <Link to="/historias">
+                  <li><a class="dropdown-item text-white" >Tus historias</a></li>
+                </Link>
+                <Link to="/psicofonias">
+                  <li><a class="dropdown-item text-white" >Psicofonías</a></li>
+                </Link>
               </ul>
             </li>
           </ul>
-          <div className="d-flex" role="search">
-            <input
-              className="form-control me-2"
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" className="form-control me-2"
               placeholder="Search"
               type="text"
               value={searchTerm}
               onChange={handleSearchTermChange}
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-success"
+              aria-label="Search" />
+            <button className="btn btn-outline-success"
               type="button"
-              onClick={handleSearchClick}
-            >
-              Search
-            </button>
-          </div>
+              onClick={handleSearchClick}>Search</button>
+          </form>
         </div>
       </div>
-    </nav>
+    </nav >
   );
 }
 
